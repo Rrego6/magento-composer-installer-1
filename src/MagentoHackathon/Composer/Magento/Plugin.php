85ff0679b27933bbc25c889b1a0c1f5bf6d6bacd
rrego6 <rrego@adobe.com>
2020-12-15 13:45:04 -0500

    MC-39824: Resolve issues with Composer 2 installation on qa02
    - Fix composer.json constraints
    - Add docblock

--- src/MagentoHackathon/Composer/Magento/Plugin.php
+++ src/MagentoHackathon/Composer/Magento/Plugin.php
@@ -336,10 +336,16 @@ AUTOLOAD;
         }
     }
 
+    /**
+     * @inheritdoc
+     */
     public function deactivate(Composer $composer, IOInterface $io)
     {
     }
 
+    /**
+     * @inheritdoc
+     */
     public function uninstall(Composer $composer, IOInterface $io)
     {
     }
